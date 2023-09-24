7e5caec4e65737b0e395c8b22883cbbc78f3194f
Serhii Hadzhilov <gadzhilov.sergey@gmail.com>
2023-09-24 20:46:08 +0300

    Remove version number from mac installer script

--- src/install/mac/mac.cmake
+++ src/install/mac/mac.cmake
@@ -4,9 +4,6 @@ set(CPACK_GENERATOR "DragNDrop")
 
 configure_file("${INSTALL_SCRIPTS}/dependencies.cmake.in" "${INSTALL_SCRIPTS}/dependencies.cmake" @ONLY)
 set(CPACK_PRE_BUILD_SCRIPTS ${INSTALL_SCRIPTS}/dependencies.cmake)
-set(CPACK_PACKAGE_VERSION_MAJOR "0")
-set(CPACK_PACKAGE_VERSION_MINOR "0")
-set(CPACK_PACKAGE_VERSION_PATCH "1")
 set(CPACK_PACKAGE_DIRECTORY "${CMAKE_INSTALL_PREFIX}/pack")
 
 include(CPACK)
